ae9198b50988ec7a47720cd6ae9ef1bc984a13e5
DEV2-4-PC <b1uet1me@naver.com>
2019-04-08 09:01:00 +0900

    -임시 추가분

---
+++ app/src/main/java/com/dutch/hdh/dutchpayapp/adapter/DutchpayPhotoDetailListAdapter.java
@@ -0,0 +1,63 @@
+package com.dutch.hdh.dutchpayapp.adapter;
+
+import android.databinding.ObservableArrayList;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.dutch.hdh.dutchpayapp.databinding.ItemDutchpayListDetailBinding;
+import com.dutch.hdh.dutchpayapp.ui.dutchpay.photodetail.DutchpayPhotoDetailContract;
+import com.dutch.hdh.dutchpayapp.ui.dutchpay.startdetail.TempDetailListModel;
+
+public class DutchpayPhotoDetailListAdapter extends RecyclerView.Adapter<DutchpayPhotoDetailListAdapter.dDetailPhotoViewHolder> {
+
+    private ObservableArrayList<TempDetailListModel> mList;
+    private DutchpayPhotoDetailContract.Presenter mDPhotoPresenter;
+
+    public class dDetailPhotoViewHolder extends RecyclerView.ViewHolder{
+
+        ItemDutchpayListDetailBinding mBinding;
+
+        public dDetailPhotoViewHolder(ItemDutchpayListDetailBinding binding) {
+            super(binding.getRoot());
+            this.mBinding = binding;
+        }
+
+        void bind(TempDetailListModel item) {
+            mBinding.setItem(item);
+
+            //mBinding.getRoot().setOnClickListener(v -> mDSatrtPresenter.onItemClick(item));
+        }
+    }
+
+    public DutchpayPhotoDetailListAdapter(ObservableArrayList<TempDetailListModel> mList, DutchpayPhotoDetailContract.Presenter mDPhotoPresenter) {
+        this.mList = mList;
+        this.mDPhotoPresenter = mDPhotoPresenter;
+    }
+
+    public void setItem(ObservableArrayList<TempDetailListModel> list){
+        if(list == null ){return;}
+        this.mList = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public dDetailPhotoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ItemDutchpayListDetailBinding binding = ItemDutchpayListDetailBinding.inflate(LayoutInflater.from(viewGroup.getContext()),viewGroup,false);
+
+        return new dDetailPhotoViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull dDetailPhotoViewHolder dDetailViewHolder, int i) {
+        TempDetailListModel item = mList.get(i);
+        dDetailViewHolder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+}
--- app/src/main/java/com/dutch/hdh/dutchpayapp/ui/dutchpay/photodetail/DutchpayPhotoDetailFragment.java
+++ app/src/main/java/com/dutch/hdh/dutchpayapp/ui/dutchpay/photodetail/DutchpayPhotoDetailFragment.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +12,28 @@ import android.view.ViewGroup;
 import com.dutch.hdh.dutchpayapp.R;
 import com.dutch.hdh.dutchpayapp.base.BaseFragment;
 import com.dutch.hdh.dutchpayapp.databinding.FragmentDutchpayPhotoDetailBinding;
+import com.dutch.hdh.dutchpayapp.ui.dutchpay.photo.DutchpayPhotoContract;
 
-public class DutchpayPhotoDetailFragment extends BaseFragment {
+public class DutchpayPhotoDetailFragment extends BaseFragment implements DutchpayPhotoContract.View {
 
     FragmentDutchpayPhotoDetailBinding mBinding;
+    DutchpayPhotoDetailPresenter mPresenter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(LayoutInflater.from(container.getContext()), R.layout.fragment_dutchpay_photo_detail,container,false);
         mBinding.setFragment(this);
+        mPresenter = new DutchpayPhotoDetailPresenter(this);
 
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void adapterInit() {
+        //어댑터 셋팅
+        mBinding.setMemberList(mPresenter.getmPDetailList());
+        mBinding.rvMemberList.setAdapter(mPresenter.getmAdapter());
+        mBinding.rvMemberList.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
     }
 }
